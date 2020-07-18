601c10365b39ff8d676acb1fa21fd3ac93680c04
joegumke <joegumke@gmail.com>
2020-07-18 15:02:01 -0500

    Add files via upload
    added support for local getlocation of maxmind database

--- arinAPI.py
+++ arinAPI.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-import requests, sys
+#Dependency -> Maxmind Geo2Country DB . Requires a free account.
+#Usage: arinRPI.py $IP 
+
+import maxminddb
+import requests, sys, json
 import xml.etree.ElementTree as ET
 
 def main(argv):
@@ -12,6 +16,16 @@ def main(argv):
     for child in root.iter('*'):
         print(child.tag,child.text)
 
+    try:
+        reader = maxminddb.open_database('GeoLite2-Country.mmdb')
+        country = reader.get(ip)
+        result = json.dumps(country, indent=4 ,sort_keys=True)
+        print(result)
+        
+    except Exception:
+        print('you dont have Maxmind DB in current Directory. Exiting....')
+        exit()
+    
 
 if __name__ == "__main__":
     main(sys.argv[1])
